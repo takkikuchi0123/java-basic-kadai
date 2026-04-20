1e622fde722a0cf1d60495110f16d110a180d77c
takkikuchi0123 <takkikuchi@aicom.co.jp>
2026-04-20 14:19:43 +0900

    継承を使って自分と家族を紹介しよう

---
+++ kadai_018/KatoExec_Chapter18.java
@@ -0,0 +1,19 @@
+package kadai_018;
+
+public class KatoExec_Chapter18 {
+
+    public static void main(String[] args) {
+
+        Kato_Chapter18[] family = {
+            new KatoTaro_Chapter18(),
+            new KatoIchiro_Chapter18(),
+            new KatoHanako_Chapter18()
+        };
+        
+        for (Kato_Chapter18 member : family) {
+            member.setGivenName();
+            member.execIntroduce();
+            System.out.println();
+        }
+    }
+}
---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,17 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18{
+	public void setGivenName() {
+		 this.givenName = "太郎";
+	}
+
+
+	public void eachIntroduce() {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("私はJavaが得意です");
+		
+	}
+	
+	
+
+}
